023ef32ca7f9c0d971f6414398e6af9fd329f199
HugoAseka <hugomaseka@gmail.com>
2022-10-09 17:34:39 -0300

    get movie by id

--- src/services/moviesService.ts
+++ src/services/moviesService.ts
@@ -6,9 +6,8 @@ const api_key = process.env.API_KEY;
 // "642885?api_key=d14c3c3285ff3f1e05204d31ea4b510b&language=en-US"
 
 export async function reqMovieById(id: number) {
-  
-  //   return await axios.get(baseUrl + id + "?api_key=" + api_key);
-  return await axios.get(
-    "https://api.themoviedb.org/3/movie/642885?api_key=d14c3c3285ff3f1e05204d31ea4b510b&language=en-US"
-  );
+  return await axios.get(baseUrl + id + "?api_key=" + api_key);
+//   return await axios.get(
+//     "https://api.themoviedb.org/3/movie/642885?api_key=d14c3c3285ff3f1e05204d31ea4b510b&language=en-US"
+//   );
 }
