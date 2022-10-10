75b6c3f25b722394c72327998879cdf7e8bcd2f9
HugoAseka <hugomaseka@gmail.com>
2022-10-10 16:43:28 -0300

    change thunder tests to heroku url

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -3,11 +3,6 @@ import { reqSearch } from "../services/searchService";
 
 export async function getSearchList(req: Request, res: Response) {
   const { query } = req.query;
-  console.log(query);
   const searchList = await reqSearch(query);
   res.status(200).send(searchList.data);
-  // const { data: movie } = await reqMovieById(id);
-  // const { data } = await reqMovieProviders(id);
-  // const providers = data.results.BR;
-  // res.status(200).send({ movie, providers });
 }
